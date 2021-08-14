b47e51bddf7384922afc33f80016cd6543ef2c9e
Rachael White <68253228+whiter9@users.noreply.github.com>
2021-08-14 02:59:40 -0500

    Update pdb_chain_select.py

--- pdb_chain_select.py
+++ pdb_chain_select.py
@@ -6,20 +6,14 @@ specified chain or chains of an existing PDB entry.
 
 from Bio.PDB import * #PDBList, PDBIO, Select
 
-
 class KeepChains(Select):
     """ Only accept the specified chains when saving. """
     def __init__(self, chain_letters):
         self.chain_letters = ch6ywd
     def accept_chain(self, chain):
         return (chain.get_id() in self.chain_letters)
+    #Ref: https://stackoverflow.com/questions/11685716/how-to-extract-chains-from-a-pdb-file
 
-    
-    #sourced from https://stackoverflow.com/questions/11685716/how-to-extract-chains-from-a-pdb-file
-        
-    
-    
-            
 ####################################################
 if __name__ == "__main__":
     
@@ -28,11 +22,9 @@ if __name__ == "__main__":
     pdb_id = str(input('\nPDB ID of target structure: '))
     keep_chains = str(input('\nLetter identifier(s) of chain(s) to keep,\
                             \nseparated by commas if multiple: '))
-    print()
-    
+   
     pdb_id = pdb_id.strip().upper()
     pdb_id_lc = pdb_id.lower()
-    
     keep_chains = keep_chains.strip().upper().split(",")
     
     # download pdb file to current dir
@@ -43,9 +35,6 @@ if __name__ == "__main__":
     parser = PDBParser(PERMISSIVE = True, QUIET = True)
     structure = parser.get_structure("{}","pdb{}.ent".format(pdb_id_lc,pdb_id_lc))
     
-    #print(type(structure))
- 
-    
     ###############################################
     # select specific chains 
     # and export to new truncated pdb file
@@ -53,7 +42,6 @@ if __name__ == "__main__":
     
     io = PDBIO()
     io.set_structure(structure)
-    
     chains_id = "".join(keep_chains)
     io.save("{}_{}.pdb".format(pdb_id,chains_id), select=KeepChains(keep_chains))
     
