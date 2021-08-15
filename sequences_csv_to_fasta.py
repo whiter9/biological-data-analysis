6485399bde23d5ac5e40b17ca3a491912b116ed6
Rachael White <68253228+whiter9@users.noreply.github.com>
2021-08-15 17:35:40 -0500

    Update sequences_csv_to_fasta.py

--- sequences_csv_to_fasta.py
+++ sequences_csv_to_fasta.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-fold = '4H'
-_4H = pd.read_csv('4H_NGS_filtered.csv', header=0)
-print(_4H)
+NGS_file = str(input('Name of csv file containing NGS sequence data => ")).strip()
+NGS_data = pd.read_csv(NGS_file, header=0)
+print(NGS_data)
 
-with open('4H_sequences.txt', 'w') as save_file:  
-    for i in range(len(_4H['seq'])):
-        save_file.write('>{}.{}\n{}\n'.format(fold,i,_4H['seq'][i]))
+for i in range(len(NGS_data['seq'])):
+    with open('ngs_seq_{}.fasta'.format(i), 'w') as save_file:  
+        save_file.write('>NGS_seq_{}\n{}\n'.format(i,NGS_data['seq'][i]))
