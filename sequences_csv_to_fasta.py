3cf7cee90c25c787b3a6781796c2878c9214ba78
Rachael White <68253228+whiter9@users.noreply.github.com>
2021-08-15 17:39:19 -0500

    Delete sequences_csv_to_fasta.py

--- sequences_csv_to_fasta.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-NGS_file = str(input('Name of csv file containing NGS sequence data => ")).strip()
-NGS_data = pd.read_csv(NGS_file, header=0)
-print(NGS_data)
-
-for i in range(len(NGS_data['seq'])):
-    with open('ngs_seq_{}.fasta'.format(i), 'w') as save_file:  
-        save_file.write('>NGS_seq_{}\n{}\n'.format(i,NGS_data['seq'][i]))
